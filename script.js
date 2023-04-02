88ea47c0091e82e2af9213eac39e5e4c434708ef
Darliene <106404807+Darli-n@users.noreply.github.com>
2023-04-02 20:29:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const number1 = document.querySelector("#number-1")
+number1.addEventListener('click', function() {
+    number1.style.backgroundColor = 'hsl(25, 97%, 53%)'
+})
+const number2 = document.querySelector("#number-2")
+number2.addEventListener('click', function() {
+    number2.style.backgroundColor = 'hsl(25, 97%, 53%)'
+})
+const number3 = document.querySelector("#number-3")
+number3.addEventListener('click', function() {
+    number3.style.backgroundColor = 'hsl(25, 97%, 53%)'
+})
+const number4 = document.querySelector("#number-4")
+number4.addEventListener('click', function() {
+    number4.style.backgroundColor = 'hsl(25, 97%, 53%)'
+})
+const number5 = document.querySelector("#number-5")
+number5.addEventListener('click', function() {
+    number5.style.backgroundColor = 'hsl(25, 97%, 53%)'
+})
